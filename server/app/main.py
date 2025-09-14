9c323efe23acf5f2115ce11f244f48671259b75c
Deepanshu12344 <deepanshu123sharma4@gmail.com>
2025-09-14 20:37:38 +0530

    server setup complete

---
+++ server/app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.routes import example
+
+app = FastAPI(title="Social")
+
+app.include_router(example.router)
+
+@app.get("/")
+def health():
+    return {"message": "everything is looking good"}
